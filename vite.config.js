c2629dee15dd380bd99a1660b343694eb6477d0d
Berat Güdelek <beratgdlk@gmail.com>
2024-12-16 00:48:13 +0300

    111

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './', // Bunu ./ olarak değiştirdik
+  build: {
+    assetsDir: 'assets', // Asset'lerin build'de nereye konumlanacağını belirtiyoruz
+    outDir: 'dist', // Build çıktı klasörü
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
